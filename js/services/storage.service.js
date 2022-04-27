87c8b53c3f5192a3e08935e392c681972143ef54
Aviv <aviv80010@gmail.com>
2022-04-27 12:19:00 +0300

    Added base file skeleton for keep and email apps

---
+++ js/services/storage.service.js
@@ -0,0 +1,7 @@
+function saveToStorage(key, val) {
+  localStorage.setItem(key, JSON.stringify(val))
+}
+function loadFromStorage(key) {
+  let val = localStorage.getItem(key)
+  return JSON.parse(val)
+}
