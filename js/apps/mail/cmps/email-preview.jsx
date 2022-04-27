efba373f6cfd6952b4f496a6000f3d3bc025a039
NoyaItzhaki <noinoya@gmail.com>
2022-04-27 16:19:04 +0300

    noya 16:19

--- js/apps/mail/cmps/email-preview.jsx
+++ js/apps/mail/cmps/email-preview.jsx
@@ -6,7 +6,6 @@ export class EmailPreview extends React.Component {
     render() {
       return (
         <main className="email-preview">
-          <h2>Email Preview</h2>
           <EmailFilter/>
           <EmailList/>
           <EmailFolderList/>
--- js/pages/app-home.jsx
+++ js/pages/app-home.jsx
@@ -1,9 +1,13 @@
+const { Link } = ReactRouterDOM
+
 export function AppHome() {
   return (
     <section className="app-home flex">
-      <h1>Welcome to apsus!</h1>
+      <h1>Welcome to appsus!</h1>
       <h3>Check us out!</h3>
-      <h4>Mail | Books | Keep</h4>
+      <h4 className="links"><Link to={'/mail'}>Mail |</Link>
+      <Link to={'/keep'}> Keep |</Link>
+      <Link to={'/books'}> Books</Link></h4>
     </section>
   )
 }
