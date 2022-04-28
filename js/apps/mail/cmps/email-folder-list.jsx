7cfca234d341ffe92dfc25302f10a847d8698b8b
NoyaItzhaki <noinoya@gmail.com>
2022-04-28 16:21:27 +0300

    noya 16:21

--- js/apps/mail/cmps/email-folder-list.jsx
+++ js/apps/mail/cmps/email-folder-list.jsx
@@ -6,7 +6,11 @@ export class EmailFolderList extends React.Component {
   render() {
       return (
         <main className="email-folder-list">
-          <h2>Email Folder List</h2>
+          
+          <h3><img className="folder-list-icon" src="assets/img/mail-icons/inbox.png" alt="" />Inbox</h3>
+          <h3><img className="folder-list-icon" src="assets/img/mail-icons/star.png" alt="" />Starred</h3>
+          <h3><img className="folder-list-icon" src="assets/img/mail-icons/sent.png" alt="" />Sent Mail</h3>
+          <h3><img className="folder-list-icon" src="assets/img/mail-icons/draft.png" alt="" />Drafts</h3>
         </main>
       )
     }
