93716ff8f2074a3ce6dab950c6f0b241b3ce41ce
NoyaItzhaki <noinoya@gmail.com>
2022-04-28 15:22:50 +0300

    noya 15:22

--- js/apps/mail/cmps/email-filter.jsx
+++ js/apps/mail/cmps/email-filter.jsx
@@ -2,7 +2,7 @@ export class EmailFilter extends React.Component {
     render() {
       return (
         <main className="email-filter">
-          <input type="text" placeholder="Search mail" />
+          <input className="filter-input" type="text" placeholder=" 🔍︎  Search mail" />
         </main>
       )
     }
