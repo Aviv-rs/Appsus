9870ed00c06c2b47c34065cfd2d2dc0e4d4629e9
NoyaItzhaki <noinoya@gmail.com>
2022-04-29 18:59:32 +0300

    noya 19:00

--- js/cmps/app-header.jsx
+++ js/cmps/app-header.jsx
@@ -1,19 +1,42 @@
 const { NavLink, withRouter } = ReactRouterDOM
 
-export function _AppHeader() {
-  return (
-    <header className="app-header flex">
-      <img src="./assets/img/logo.png" className="header-image"/>
-      <nav className="page-nav flex">
-        <NavLink  className="page-link" exact to="/">
+export class _AppHeader extends React.Component {
+
+  state = {
+    isModalOpen: false
+  }
+
+  onToggleModal = () => {
+    this.setState({ isModalOpen: !this.state.isModalOpen })
+  }
+
+  render() {
+    let { isModalOpen } = this.state
+    return (
+      <header className="app-header flex">
+        <NavLink exact to="/"><img src="./assets/img/logo.png" className="header-image" /></NavLink>
+        <nav className="page-nav flex">
+          <img onClick={this.onToggleModal} className="page-link" src="./assets/img/menu.png" alt="" />
+          {isModalOpen &&
+            <div>
+              <NavLink exact to="/"><img onClick={this.onToggleModal} src="./assets/img/home.png" alt="" /></NavLink>
+              <NavLink  to="/about"><img onClick={this.onToggleModal} src="./assets/img/about.png" alt="" /></NavLink>
+              <NavLink  to="/keep"><img onClick={this.onToggleModal} src="./assets/img/note.png" alt="" /></NavLink>
+              <NavLink  to="/mail"><img onClick={this.onToggleModal} src="./assets/img/email.png" alt="" /></NavLink>
+              <NavLink  to="/books"><img onClick={this.onToggleModal} src="./assets/img/book.png" alt="" /></NavLink>
+            </div>
+          }
+
+          {/* <NavLink  className="page-link" exact to="/">
           Home
-        </NavLink>
-        <NavLink  className="page-link"  to="/about">
+          </NavLink>
+          <NavLink  className="page-link"  to="/about">
           About
-        </NavLink>
-      </nav>
-    </header>
-  )
+        </NavLink> */}
+        </nav>
+      </header>
+    )
+  }
 }
 
 export const AppHeader = withRouter(_AppHeader)
