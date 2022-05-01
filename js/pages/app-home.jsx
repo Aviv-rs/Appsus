bdab597c72b01441a5f3481eb445aed560f1182f
Aviv <aviv80010@gmail.com>
2022-05-01 19:58:33 +0300

    added home bg animation and resized hero

--- js/pages/app-home.jsx
+++ js/pages/app-home.jsx
@@ -2,22 +2,38 @@ const { Link } = ReactRouterDOM
 
 export function AppHome() {
   return (
-    <section className="app-home flex">
+    <section className="app-home ">
       <object
         className="hero-home"
-        data="assets\img\hero.svg"
-        width="800"
-        height="800"
+        data="assets\img\hero-cropped.svg"
+        width="600"
+        height="600"
       >
         {' '}
       </object>
-      <main>
-        <h1>Welcome to Appsus!</h1>
-        <h3>Check us out!</h3>
-        <h4 className="links">
-          <Link to={'/mail'}>Mail |</Link>
-          <Link to={'/keep'}> Keep |</Link>
-          <Link to={'/books'}> Books</Link>
+      <div className="bg-circle-container">
+        <img
+          className="home-bg-circle rotate-center"
+          src="assets\img\background-circle.png"
+          alt="animated background circle"
+        />
+      </div>
+      <main className="welcome-section">
+        <p className="welcome-to-txt">Welcome to</p>
+        <h1 className="appsus-title">Appsus</h1>
+        <h3 className="explore-apps-txt">Explore our apps</h3>
+        <h4 className="home-app-links">
+          <Link className="home-app-link" to={'/mail'}>
+            Mail
+          </Link>
+          <Link className="home-app-link" to={'/keep'}>
+            {' '}
+            Keep
+          </Link>
+          <Link className="home-app-link" to={'/books'}>
+            {' '}
+            Books
+          </Link>
         </h4>
       </main>
     </section>
